ebf0233d442cb6508d03ecaa0579dbaf9ab1a413
Eslam Dweeb <Eslamdweeb12@gmail.com>
2025-03-02 21:27:59 +0200

    Initial Commit

---
+++ CeretMobileTAsk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CeretMobileTAsk
+//
+//  Created by eslam dweeb on 02/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
